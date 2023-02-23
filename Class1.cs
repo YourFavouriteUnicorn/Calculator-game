85a8d88c97cd73145f697a575793c55c364cd339
Egle.S <egle9416@gmail.com>
2023-02-23 19:41:31 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleProject
+{
+	class Class1
+	{
+		static void Main()
+		{
+			System.Diagnostics.Debug.WriteLine("Hello World");
+		}
+	}
+}
---
+++ ExampleProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ExampleProject
+{
+    class Program
+    {
+
+        
+
+static void Main(string[] args)
+        {
+
+            void answerFunction(string inputName)
+            {
+                if (inputName.ToUpper() == "YES")
+                {
+                    Console.WriteLine("Let's begin!");
+                    Console.ReadLine();
+                }
+                else if (inputName.ToUpper() == "NO")
+                {
+                    Console.WriteLine("Hope to see you next time, goodbye!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Ooops something went wrong, please type correct answer into console");
+                    Console.ReadLine();
+                }
+            }
+
+
+
+            Console.Write("hello, what is your name?");
+            string name = Console.ReadLine();
+            
+            Console.Write("Hello " + name + " Do you want to calculate some numbers? Please type \"yes\" or \"no\"");
+            string answer = Console.ReadLine();
+              answerFunction(answer);
+            
+            
+
+
+
+           
+
+        }
+    }
+}
